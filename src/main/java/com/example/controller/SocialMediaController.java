b91a1821585535034fb7901f7977b4d761db6ac1
Eddie Chen <somestuff14@yahoo.com>
2025-04-18 04:23:46 +0000

    Cleaning up SocialMediaController

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,17 +1,7 @@
 package com.example.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.example.entity.Account;
-import com.example.entity.Message;
 import com.example.service.AccountService;
 import com.example.service.MessageService;
 
