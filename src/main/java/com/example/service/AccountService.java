ec9c7fb0eca94181fd03fa7d47ad4cd2d33e0ba9
Eddie Chen <somestuff14@yahoo.com>
2025-04-18 04:16:24 +0000

    Cleaned up Service classes

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,19 +1,11 @@
 package com.example.service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 import javax.security.sasl.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.example.entity.Account;
 import com.example.exception.DuplicateUsernameException;
